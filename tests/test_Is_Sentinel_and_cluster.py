4a3d585a843b8655cf074c9b2ae2a436c89cd026
Muhammad Qadora <memomq70@gmail.com>
2024-11-20 19:41:03 +0200

    fixed path

--- tests/test_Is_Sentinel_and_cluster.py
+++ tests/test_Is_Sentinel_and_cluster.py
@@ -1,7 +1,7 @@
 import pytest
 from unittest.mock import MagicMock
-from ..falkordb.falkordb import Is_Cluster
-from ..falkordb.falkordb import Is_Sentinel
+from .falkordb.falkordb import Is_Cluster
+from .falkordb.falkordb import Is_Sentinel
 import redis
 
 @pytest.fixture
