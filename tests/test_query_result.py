0b378ebaa1a85f9c1c53910cb157d7f9b1c37900
Guy Korland <gkorland@gmail.com>
2026-02-14 21:31:39 +0200

    Update test_query_result.py

--- tests/test_query_result.py
+++ tests/test_query_result.py
@@ -1,6 +1,5 @@
 import pytest
 from unittest.mock import Mock
-from datetime import datetime, date, time
 from dateutil.relativedelta import relativedelta
 from collections import OrderedDict
 from redis import ResponseError
